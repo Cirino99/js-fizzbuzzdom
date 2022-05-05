2d099d3cfafeadb97a91dfe63dffb67755c600d7
Simone <cirino.simone99@gmail.com>
2022-05-05 15:42:10 +0200

    sito finito

--- js/script.js
+++ js/script.js
@@ -3,20 +3,28 @@
     Buzz per i multipli di 5 e FizzBuzz per i multipli di entrambi (3 e 5).
 */
 
-for(let i=1; i<101; i++){
+const mybox = document.getElementById('mainbox');
+let element = `<div class="box">1</div>`;
+for(let i=2; i<101; i++){
     if(i%3===0){
         if(i%5===0){
+            element += `<div class="box fizzbuzz">FizzBuzz</div>`;
             console.log('Iterazione: FizzBuzz');
         } else {
+            element += `<div class="box fizz">Fizz</div>`;
             console.log('Iterazione: Fizz');
         }
     } else if(i%5===0){
         if(i%3===0){
+            element += `<div class="box fizzbuzz">FizzBuzz</div>`;
             console.log('Iterazione: FizzBuzz');
         } else {
+            element += `<div class="box buzz">Buzz</div>`;
             console.log('Iterazione: Buzz');
         }
     } else {
+        element += `<div class="box">${i}</div>`;
         console.log('Iterazione:',i);
     }
 }
+mybox.innerHTML += element;
